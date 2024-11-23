f4a1a33bffb611c582e1af22ddf5e28dab4b629d
tjq19842003 <chuckeytang@gmail.com>
2024-11-23 17:42:42 +0800

    前端大屏结构构建

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import path from "path";
 import createVitePlugins from "./vite/plugins";
 import fs from "fs";
+import Importer from "vite-plugin-importer";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode, command }) => {
@@ -12,7 +13,14 @@ export default defineConfig(({ mode, command }) => {
     // 默认情况下，vite 会假设你的应用是被部署在一个域名的根路径上
     // 例如 https://www.ruoyi.vip/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.ruoyi.vip/admin/，则设置 baseUrl 为 /admin/。
     base: VITE_APP_ENV === "production" ? "/" : "/",
-    plugins: createVitePlugins(env, command === "build"),
+    plugins: [
+      createVitePlugins(env, command === "build"),
+      Importer({
+        libraryName: "ant-design-vue",
+        libraryDirectory: "es",
+        style: (name) => `ant-design-vue/es/${name}/style/index.less`,
+      }),
+    ],
     resolve: {
       // https://cn.vitejs.dev/config/#resolve-alias
       alias: {
@@ -44,6 +52,17 @@ export default defineConfig(({ mode, command }) => {
     },
     //fix:error:stdin>:7356:1: warning: "@charset" must be the first rule in the file
     css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true, // 启用 JavaScript 处理
+          modifyVars: {
+            "primary-color": "#1890ff", // 主色
+            "link-color": "#1DA57A", // 链接色
+            "border-radius-base": "4px", // 圆角
+            // 根据需求修改其他主题变量
+          },
+        },
+      },
       postcss: {
         plugins: [
           {
